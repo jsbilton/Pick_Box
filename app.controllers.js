d51cd33a42b512eec96ff9d822b752b7346e1e23
Jared Bilton <cantops@jareds-mbp.attlocal.net>
2015-11-24 20:47:18 -0500

    Initial Commits

---
+++ app.controllers.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+  angular
+  .module('pbox')
+  .controller('MainController', function ($scope) {
+
+    })
+  .controller('PboxController', function ($scope, PboxService, LikeService, $routeParams) {
+    PboxService.getPboxData().then(function (pbox) {
+
+      $scope.pbox = pbox; //attaching pbox with scope
+    });
+    LikeService.getLikes().success(function (likedAccessories) {
+
+      $scope.likedAccessories = likedAccessories;
+    })
+    $scope.likeAccessory = function (accessory) {
+    LikeService.addLikedAccessory(accessory);
+    };
+    if ($routeParams, likedPboxId) { //next is an ajax call
+      LikeService.getSingleAccessory($routeParams.likedPboxId).success(function (likedItem) {
+        $scope.likedItem = likedItem;
+        console.log(likedItem);
+      })
+    }
+  })
+  .controller('CustController', function ($scope, PboxService, CartService) {
+    // body...
+  })
+
+
+}());
