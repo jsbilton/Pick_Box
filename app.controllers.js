401077596e99d9168fd2e82ab1301c2c54a8e7d3
Jared Bilton <cantops@jareds-mbp.attlocal.net>
2015-11-25 07:46:09 -0500

    adding services

--- app.controllers.js
+++ app.controllers.js
@@ -5,16 +5,44 @@
   .controller('MainController', function ($scope) {
 
     })
+
+    // Admin Controller = PboxController
+    .controller('AdminController', function ($scope, PboxService, $routeParams)
+
+      PboxService.getPbox().success(function (box){
+        console.log(box);
+        $scope.msg = box;
+      });
+
+      var clearInput = function (box){
+        $scope.addAccessories.title = " ";
+        $scope.addAccessories.image = " ";
+        $scope.addAccessories.price = " ";
+        $scope.addAccessories.description = " ";
+      };
+      $scope.gear = function (addAccessories) {
+        setTimeout(clearInput, 100);
+        prompt('Rock Hard');
+      };
+      $scope.delPbox = function (box) {
+        PboxService.deletePbox(box);
+      }
+    })
+
     // Customer Controller = CustController
   .controller('CustController', function ($scope, PboxService, CartService, $routeParams) {
-    PboxService.getPboxData().then(function (pbox) {
+
+    PboxService.getPboxData().then(function (box) {
 
       $scope.pbox = pbox; //attaching pbox with scope
     });
+
+    //demo code below
     // CartService.getLikes().success(function (likedAccessories) {
     //
     //   $scope.likedAccessories = likedAccessories;
     // })
+    
     $scope.likeAccessory = function (accessory) {
     CartService.addCartAccessory(accessory);
     };
@@ -25,17 +53,15 @@
       })
     }
   })
-  // Admin Controller = PboxController
-  .controller('AdminController', function ($scope, PboxService, $routeParams)
-
-    if ($routeParams.like) {
-
-  }{
-
-  })
 
   .controller('CartController', function ($scope, CartService, $routeParams) {
-    CartService.get
+    CartService.getPbox().success(function(gear){
+      $scope.msg = accessories;
+    });
+    $scope.delCartAccessory = function (pboxItem) {
+      CartService.delCartAccessory(pboxItem);
+    };
+
   })
 
 
--- app.services.js
+++ app.services.js
@@ -1,4 +1,23 @@
 (function() {
   'use strict';
+   angular
+    .module('pbox')
+    .factory('PboxService', function($http) {
+      var url = 'http://tiny-tiny.herokuapp.com/collections/pbox';
+
+
+      var addPbox = function (addAccessories) {
+             $http.post(url, addAccessories).then(function (res) {
+               console.log(res);
+             });
+          };
+
+      var getPbox = function () {
+        return $http.get(url);
+      };
+
+      var removePbox = function (box){
+        $http.delete (url + '/' + box._id);
+      };
 
 }());
